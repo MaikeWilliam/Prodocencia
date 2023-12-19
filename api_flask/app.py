e021d3a40049d1f317b55a19af726fd0fa0b6194
Maike William <maikewilliamb@gmail.com>
2023-12-19 17:23:44 -0300

    Alteração nas métricas

--- api_flask/app.py
+++ api_flask/app.py
@@ -4,123 +4,96 @@ from flask_cors import CORS
 # Numpy
 import numpy as np
 
-#Einsteinpy
+# Einsteinpy
 import sympy
 from sympy import symbols, sin, Function, diag
-from einsteinpy.symbolic import (MetricTensor,
-                                 RicciTensor, RicciScalar,
-                                 RiemannCurvatureTensor)
+from einsteinpy.symbolic import (
+    MetricTensor,
+    RicciTensor,
+    RicciScalar,
+    RiemannCurvatureTensor,
+)
 
-from einsteinpy.symbolic.predefined import (AlcubierreWarp, AntiDeSitter,
-                                            AntiDeSitterStatic,
-                                            BarriolaVilekin, BertottiKasner,
-                                            BesselGravitationalWave, CMetric,
-                                            DeSitter, Ernst, Godel,
-                                            JanisNewmanWinicour, Kerr,
-                                            KerrNewman, Minkowski,
-                                            MinkowskiCartesian, MinkowskiPolar,
-                                            ReissnerNordstorm, Schwarzschild)
-
+from einsteinpy.symbolic.predefined import Kerr, KerrNewman, Schwarzschild
 
 
 # Criando aplicação
 app = Flask(__name__)
 CORS(app)
 
-# Rota Default
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({ "message": "Hello world" })
 
-@app.route('/metricas' , methods=['GET'])
+# Rota Default
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"message": "Hello world"})
+
+
+@app.route("/metricas", methods=["GET"])
 def get_metricas():
     metricas = [
-        {'value' :'Schwarzschild'}, {'value' :'AntiDeSitter'}, {'value' :'AntiDeSitterStatic'}, {'value' :'BarriolaVilekin'}, {'value' :'BertottiKasner'}, {'value' :'BesselGravitationalWave'}, {'value' :'CMetric'}, {'value' :'DeSitter'}, {'value' :'Ernst'}, {'value' :'Godel'}, {'value' :'JanisNewmanWinicour'}, {'value' :'Kerr'}, {'value' :'KerrNewman'}, {'value' :'Minkowski'}, {'value' :'MinkowskiCartesian'}, {'value' :'MinkowskiPolar'}, {'value' :'ReissnerNordstorm'}, {'value' :'AlcubierreWarp'}, {'value': 'FLRW'}
+        {"value": "Schwarzschild"},
+        {"value": "Kerr"},
+        {"value": "KerrNewman"},
+        {"value": "FLRW"},
     ]
     return jsonify(metricas)
 
-# Rota Tensores
-@app.route('/tensores', methods=['POST']) 
-def calcular_tensores():
 
-    metrica = request.json['metrica']
-    tipo = request.json['tipo']
-    
+# Rota Tensores
+@app.route("/tensores", methods=["POST"])
+def calcular_tensores():
+    metrica = request.json["metrica"]
+    tipo = request.json["tipo"]
+
     tensor = Tensor(metric=metrica)
-    
-    if tipo == 'tensor':
+
+    if tipo == "tensor":
         result = tensor.get_tensor()
-    elif tipo == 'riemann':
+    elif tipo == "riemann":
         result = tensor.get_riemann_tensor()
-    elif tipo == 'ricci':
+    elif tipo == "ricci":
         result = tensor.get_ricci_tensor()
-    elif tipo == 'ricciScalar':
+    elif tipo == "ricciScalar":
         result = tensor.get_ricci_scalar()
     else:
-        return jsonify(error='Tipo de tensor inválido')
-    
+        return jsonify(error="Tipo de tensor inválido")
+
     return jsonify(result=result)
 
-class Tensor():
-    def __init__(self, metric='Schwarzschild'):
+
+class Tensor:
+    def __init__(self, metric="Schwarzschild"):
         self.__metric = self.__get_metric(metric)
 
     def __get_metric(self, metric_name):  # noqa: C901
         """Retorna uma metrica prédefinida usando o nome informado."""
-        if metric_name == 'AlcubierreWarp':
-            return AlcubierreWarp()
-        elif metric_name == 'AntiDeSitter':
-            return AntiDeSitter()
-        elif metric_name == 'AntiDeSitterStatic':
-            return AntiDeSitterStatic()
-        elif metric_name == 'BarriolaVilekin':
-            return BarriolaVilekin()
-        elif metric_name == 'BertottiKasner':
-            return BertottiKasner()
-        elif metric_name == 'BesselGravitationalWave':
-            return BesselGravitationalWave()
-        elif metric_name == 'CMetric':
-            return CMetric()
-        elif metric_name == 'DeSitter':
-            return DeSitter()
-        elif metric_name == 'Ernst':
-            return Ernst()
-        elif metric_name == 'FLRW':
+        if metric_name == "FLRW":
             # Definir a constante k
-            k = symbols('k')
+            k = symbols("k")
 
-            a = Function('a')
+            a = Function("a")
             # Definir as variaveis das metricas
             syms = symbols("t  r theta phi")
             t, r, th, ph = syms
 
             # Definir o tensor da métrica
-            m = diag(-1, (a(t) ** 2) / (1-k * (r ** 2)),
-                     ((a(t) ** 2) * (r ** 2)),
-                     (((a(t) ** 2) * ((r * sin(th)) ** 2)))).tolist()
+            m = diag(
+                -1,
+                (a(t) ** 2) / (1 - k * (r**2)),
+                ((a(t) ** 2) * (r**2)),
+                (((a(t) ** 2) * ((r * sin(th)) ** 2))),
+            ).tolist()
 
             return MetricTensor(m, syms)
-        elif metric_name == 'Godel':
-            return Godel()
-        elif metric_name == 'JanisNewmanWinicour':
-            return JanisNewmanWinicour()
-        elif metric_name == 'Kerr':
+        elif metric_name == "Kerr":
             return Kerr()
-        elif metric_name == 'KerrNewman':
+        elif metric_name == "KerrNewman":
             return KerrNewman()
-        elif metric_name == 'Minkowski':
-            return Minkowski()
-        elif metric_name == 'MinkowskiCartesian':
-            return MinkowskiCartesian()
-        elif metric_name == 'MinkowskiPolar':
-            return MinkowskiPolar()
-        elif metric_name == 'ReissnerNordstorm':
-            return ReissnerNordstorm()
-        elif metric_name == 'Schwarzschild':
+        elif metric_name == "Schwarzschild":
             return Schwarzschild()
         else:
-            raise ValueError('Metrica não implementada.')
-            
+            raise ValueError("Metrica não implementada.")
+
     def get_tensor(self):
         """Retorna o tensor da metrica.
 
@@ -152,6 +125,7 @@ class Tensor():
             [[[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], [[0, r_s/(r**2*(r - r_s)), 0, 0], [-r_s/(r**2*(r - r_s)), 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, -r_s/(2*r), 0], [0, 0, 0, 0], [r_s/(2*r), 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, -r_s*sin(theta)**2/(2*r)], [0, 0, 0, 0], [0, 0, 0, 0], [r_s*sin(theta)**2/(2*r), 0, 0, 0]]], [[[0, r_s*c**2*(r - r_s)/r**4, 0, 0], [r_s*c**2*(-r + r_s)/r**4, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, -r_s/(2*r), 0], [0, r_s/(2*r), 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, -r_s*sin(theta)**2/(2*r)], [0, 0, 0, 0], [0, r_s*sin(theta)**2/(2*r), 0, 0]]], [[[0, 0, r_s*c**2*(-r + r_s)/(2*r**4), 0], [0, 0, 0, 0], [r_s*c**2*(r - r_s)/(2*r**4), 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, r_s/(2*r**2*(r - r_s)), 0], [0, -r_s/(2*r**2*(r - r_s)), 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, r_s*sin(theta)**2/r], [0, 0, -r_s*sin(theta)**2/r, 0]]], [[[0, 0, 0, r_s*c**2*(-r + r_s)/(2*r**4)], [0, 0, 0, 0], [0, 0, 0, 0], [r_s*c**2*(r - r_s)/(2*r**4), 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, r_s/(2*r**2*(r - r_s))], [0, 0, 0, 0], [0, -r_s/(2*r**2*(r - r_s)), 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, -r_s/r], [0, 0, r_s/r, 0]], [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]]]
         """
         return str(RiemannCurvatureTensor.from_metric(self.__metric).tensor())
+
 
 if __name__ == "__main__":
     app.run()
