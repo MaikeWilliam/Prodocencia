53ce6cec5eae6a36e53ab057ca7edcc8a8d16672
Maike William <maikewilliamb@gmail.com>
2024-08-22 18:50:37 -0300

    v1.0.0

--- front-end/project-interface/src/components/Tensores/Tensores.jsx
+++ front-end/project-interface/src/components/Tensores/Tensores.jsx
@@ -5,7 +5,7 @@ import Card from "../Card/Card";
 import api from "axios";
 
 // URL do backend com o IP e a porta corretos
-const backendUrl = "http://10.0.0.106:8132";
+const backendUrl = process.env.VITE_API_URL;
 
 export default function Tensores() {
     const [metricas, setMetricas] = useState([]);
